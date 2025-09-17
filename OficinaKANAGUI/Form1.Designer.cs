f380fe87b9316327d5e5c08e7566aae25a5a9b31
Kailane Morgado <kaaholiveira2504@gmail.com>
2025-09-16 22:07:00 -0300

    Sistema de Gerenciamento de OS Mecanica
    Implementação de um sistema de gerenciamento em C# para uma oficina mecanica.

--- OficinaKANAGUI/Form1.Designer.cs
+++ OficinaKANAGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OficinaKANAGUI
 {
-    partial class Form1
+    partial class FormInicial
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblInicial = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInicial
+            // 
+            this.lblInicial.AutoSize = true;
+            this.lblInicial.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblInicial.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.lblInicial.Location = new System.Drawing.Point(124, 29);
+            this.lblInicial.Name = "lblInicial";
+            this.lblInicial.Size = new System.Drawing.Size(248, 31);
+            this.lblInicial.TabIndex = 0;
+            this.lblInicial.Text = "OFICINA KANAGUI";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.lblEmail.Location = new System.Drawing.Point(28, 96);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(74, 27);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.BackColor = System.Drawing.Color.Silver;
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmail.Location = new System.Drawing.Point(108, 96);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(296, 31);
+            this.txbEmail.TabIndex = 2;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(26, 219);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(76, 27);
+            this.lblSenha.TabIndex = 3;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.BackColor = System.Drawing.Color.Silver;
+            this.txbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSenha.Location = new System.Drawing.Point(108, 215);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(296, 31);
+            this.txbSenha.TabIndex = 4;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogar.Location = new System.Drawing.Point(165, 260);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(122, 37);
+            this.btnLogar.TabIndex = 5;
+            this.btnLogar.Text = "Logar";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // FormInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(235)))));
+            this.BackgroundImage = global::OficinaKANAGUI.Properties.Resources.Sujo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(453, 344);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblInicial);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormInicial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela de Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInicial;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Button btnLogar;
     }
 }
 
--- OficinaKANAGUI/Form1.cs
+++ OficinaKANAGUI/Form1.cs
@@ -4,17 +4,62 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OficinaKANAGUI
 {
-    public partial class Form1 : Form
+    public partial class FormInicial : Form
     {
-        public Form1()
+        public FormInicial()
         {
             InitializeComponent();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            //Verificar se a pessoa digitou o email e a senha:
+            if (txbEmail.Text.Length < 6) //o .length eu vejo quantas letras tem no text box
+            {
+                MessageBox.Show("Digite um e-mail válido!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txbSenha.Text.Length < 4)
+            {
+                MessageBox.Show("Digite uma senha válida!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //Prosseguir...
+                Model.Usuario usuario = new Model.Usuario();
+
+                //Colocar os valores dos campos nos atributos do usuario:
+                usuario.Email = txbEmail.Text;
+                usuario.Senha = txbSenha.Text;
+
+                //Tabela que vai receber o resultado do SELECT (Logar)
+                DataTable resultado = usuario.Logar();
+
+                //Verificar se acertou o email e a senha:
+                if (resultado.Rows.Count == 0)
+                {
+                    MessageBox.Show("E-mail e/ou senha inválidos!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    //Armazenar as infos vindas do banco de dados no objeto "usuario"
+                    usuario.Id = int.Parse(resultado.Rows[0]["id"].ToString());
+                    usuario.NomeCompleto = resultado.Rows[0]["nome_completo"].ToString();
+
+                    //Mudar para o MenuPrincipal:
+                    MenuPrincipal menuPrincipal = new MenuPrincipal();
+                    Hide(); //esconder a janela atual (que no caso é a de login)
+                    menuPrincipal.ShowDialog(); //mostrar o MenuPrincipal
+
+                    Show(); //Mostrar a tela de login caso a pessoa saia do menu principal.
+                }
+            }
         }
     }
 }
